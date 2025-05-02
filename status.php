54850cf8ac01374da8c890e06ed68e4605dd7d0e
blackice282 <cascio.roberto82@gmail.com>
2025-05-02 09:16:15 +0200

    Update status.php

--- status.php
+++ status.php
@@ -1,13 +1,6 @@
 <?php
 require_once __DIR__ . '/config.php';
 header('Content-Type: text/plain');
-
 $job  = preg_replace('/[^a-z0-9]/','', $_GET['job'] ?? '');
 $path = getConfig('paths.temp') . "/progress_{$job}.log";
-
-if (file_exists($path)) {
-    echo file_get_contents($path);
-} else {
-    echo "Nessun job trovato: $job\n";
-}
-?>
+echo file_exists($path) ? file_get_contents($path) : "Nessun job $job\n";
