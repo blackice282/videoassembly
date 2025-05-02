1a6a08911c86523410b44d2b462a0f37a56e288a
blackice282 <cascio.roberto82@gmail.com>
2025-05-02 07:41:11 +0200

    Update video_effects.php

--- video_effects.php
+++ video_effects.php
@@ -1,12 +1,21 @@
 <?php
 require_once 'config.php';
+
 function applyVideoEffect($in, $out, $effect) {
     switch ($effect) {
-        case 'bw': $filter = 'hue=s=0'; break;
-        case 'vintage': $filter = 'colorchannelmixer=.393:.769:.189:0:.349:.686:.168:0:.272:.534:.131'; break;
-        case 'contrast': $filter = 'eq=contrast=1.5:brightness=0.05'; break;
-        default: return copy($in, $out);
+        case 'bw':
+            $filter = 'hue=s=0';
+            break;
+        case 'vintage':
+            $filter = 'colorchannelmixer=.393:.769:.189:0:.349:.686:.168:0:.272:.534:.131';
+            break;
+        case 'contrast':
+            $filter = 'eq=contrast=1.5:brightness=0.05';
+            break;
+        default:
+            return copy($in, $out);
     }
+
     $cmd = sprintf(
         '%s -y -threads 0 -preset ultrafast -i %s -vf "%s" -c:a copy %s',
         FFMPEG_PATH,
@@ -17,4 +26,3 @@ function applyVideoEffect($in, $out, $effect) {
     shell_exec($cmd);
     return file_exists($out) && filesize($out) > 0;
 }
-?>
