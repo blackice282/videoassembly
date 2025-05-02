b0c74e87ec0e622bc0ea2a279d1d8fa28812f7c5
blackice282 <cascio.roberto82@gmail.com>
2025-05-02 08:19:11 +0200

    Update transitions.php

--- transitions.php
+++ transitions.php
@@ -1,7 +1,5 @@
 <?php
-// transitions.php - Stub per includere le funzioni di concatenazione/transizioni
 require_once 'config.php';
 require_once 'ffmpeg_script.php';
-
-// Qui puoi aggiungere altre funzioni di transizione in futuro.
+// Questo file rimane vuoto a meno di future estensioni.
 ?>
