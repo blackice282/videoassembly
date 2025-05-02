3b02d5321816fb03bb9e6548d94b90ae18d137c1
blackice282 <cascio.roberto82@gmail.com>
2025-05-02 07:42:09 +0200

    Update ffmpeg_script.php

--- ffmpeg_script.php
+++ ffmpeg_script.php
@@ -1,12 +1,17 @@
 <?php
-function applyTransitions($inputs, $out){
+require_once 'config.php';
+
+function applyTransitions($inputs, $out) {
     $list = TEMP_DIR . '/concat_' . uniqid() . '.txt';
-    $txt = implode("\n", array_map(fn($p)=>"file '".str_replace("'","\\'",\$p)."'", \$inputs));
+    $txt  = implode("\n", array_map(function($p){
+        return "file '" . str_replace("'", "\\'", $p) . "'";
+    }, $inputs));
     file_put_contents($list, $txt);
     shell_exec(sprintf(
         '%s -y -threads 0 -preset ultrafast -f concat -safe 0 -i %s -c copy %s',
-        FFMPEG_PATH,escapeshellarg($list),escapeshellarg($out)
+        FFMPEG_PATH,
+        escapeshellarg($list),
+        escapeshellarg($out)
     ));
     unlink($list);
 }
-?>
